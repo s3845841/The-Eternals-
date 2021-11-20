d78d309b9469789da24ee8354b8d9c1daad81bb8
Jeremy Ng Kwik Tung <s3842858@student.rmit.edu.au>
2021-11-20 19:27:19 +1100

    Added Home Page Front End

--- login/index.php
+++ login/index.php
@@ -4,7 +4,7 @@
         <title>Login</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">   
-        <link rel="stylesheet" type="text/css" href="styles/loginrego.css"> 
+        <link rel="stylesheet" type="text/css" href="../styles/loginrego.css"> 
     </head>
     <body>
         Login Page
