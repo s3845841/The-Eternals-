f3f5be50e556d3ac2a276f9531d44e60d68e585d
Jeremy Ng Kwik Tung <s3842858@student.rmit.edu.au>
2021-11-21 04:07:45 +1100

    View all chat implemented

--- chats/index.php
+++ chats/index.php
@@ -1,5 +1,15 @@
 <?php require_once('../model/user.php'); ?>
+<?php require_once('../model/post.php'); ?>
+<?php require_once('../model/chat.php'); ?>
 <?php require_once('../includes/functions.inc.php'); ?>
+<?php require_once('../crud/chatCrud.php'); ?>
+<?php require_once('../crud/postCrud.php'); ?>
+<?php
+    $currentUser = getLoggedInUser();
+    $postCrud = new PostCrud();
+    $chatCrud = new ChatCrud();
+    $allChats = $chatCrud->getChatsByMentor($currentUser->getEmail());
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -18,7 +28,33 @@
     <br>
     <article>
         <h1 class="aligncenter">Chats</h1>
-
+        <br>
+        <?php if (count($allChats) > 0) { ?>
+                <table>
+                    <?php foreach ($allChats as $chat): ?>
+                        <?php
+                        $postDetails = $postCrud->getPostById($chat->getPostId());
+                        ?>
+                        <tr onclick="window.location='chat/?chat_id=<?php echo $chat->getChatId(); ?>&post_id=<?php echo $chat->getPostId(); ?>&mentee_id=<?php echo $chat->getMentee(); ?>&mentor_id=<?php echo $chat->getMentor(); ?>'">
+                            <td>
+                                <b>Mentee: <?php echo $chat->getMentee(); ?></b>
+                                <p>Subjects: <?php echo $postDetails->getSubject(); ?></p>
+                            </td>
+                            <td>
+                            <?php if ($chat->getAddress() != null) { ?>
+                                <p>Area: <?php echo $chat->getAddress(); ?></p>
+                            <?php } ?>
+                            </td>
+                            <td>
+                                <p><?php echo date('d M Y', $chat->getTimestamp()); ?></p>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+            <?php } else { ?>
+                <h2 class="aligncenter">You have no chat.</h2>
+    
+            <?php } ?>
     </article>
     </body>
 </html>
